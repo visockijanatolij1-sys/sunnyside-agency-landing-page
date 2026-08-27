cc465088e30de09c86937206e781f3d06b718f4b
Анатолій <visockijanatolij1@gmail.com>
2026-08-27 22:33:27 +0200

    finish tranform section

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 const menu = document.querySelector(".menu");
 const button = document.querySelector(".burger-icon");
+const heroTitle = document.querySelector(".hero-title");
 
 button.addEventListener("click", () => {
     menu.classList.toggle("open-menu");
     button.classList.toggle("active");
+
+    heroTitle.classList.toggle("none");
 });
