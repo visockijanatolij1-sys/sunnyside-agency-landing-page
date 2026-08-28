caa81183b5991a7b411e4e7fbdfa1f3e92e270e2
Анатолій <visockijanatolij1@gmail.com>
2026-08-28 21:32:49 +0200

    finish standout section

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 const menu = document.querySelector(".menu");
 const button = document.querySelector(".burger-icon");
 const heroTitle = document.querySelector(".hero-title");
+const heroArrow = document.querySelector(".hero-arrow");
 
 button.addEventListener("click", () => {
     menu.classList.toggle("open-menu");
     button.classList.toggle("active");
 
     heroTitle.classList.toggle("none");
+    heroArrow.classList.toggle("none");
 });
